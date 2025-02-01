bf920fd4fbf39c9da617433adb60dd3380942e69
Ram3ez <rameez12m@gmail.com>
2025-02-01 18:46:40 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,57 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(MainApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MainPage(),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return (MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Game"),
-        ),
-        body: Text("hello"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),
+        centerTitle: false,
       ),
-    ));
+      body: Center(
+        child: MaterialButton(
+          color: Colors.blue,
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SecondPage(),
+                ));
+          },
+          child: Text("Go to second Page"),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Second Page"),
+        backgroundColor: Colors.red,
+        automaticallyImplyLeading: false,
+      ),
+      body: MaterialButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Center(child: Text("Go back")),
+      ),
+    );
   }
 }
