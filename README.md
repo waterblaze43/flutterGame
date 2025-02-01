1be1928136b98782506dbf20514ee05c4991b705
Ram3ez <rameez12m@gmail.com>
2025-02-01 17:14:38 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# game
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Game"),
+        ),
+        body: Text("hello"),
+      ),
+    ));
+  }
+}
