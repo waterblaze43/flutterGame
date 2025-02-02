a55521fde14e5d66d1c8773b21dc989047b9097a
Ram3ez <rameez12m@gmail.com>
2025-02-02 11:55:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: MainPage(),
+    home: GamePage(),
   ));
 }
 
@@ -33,21 +34,130 @@ class MainPage extends StatelessWidget {
 
   Widget button(BuildContext context) {
     return MaterialButton(
-      onPressed: () {},
+      onPressed: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => GamePage()));
+      },
       height: 150,
       minWidth: 150,
       color: Colors.white,
       shape: CircleBorder(),
-      child: Text("Start"),
+      child: Text("START"),
     );
   }
 }
 
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
   GamePage({super.key});
 
   @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  double heightA = 0;
+  double heightB = 0;
+  int scoreA = 0;
+  int scoreB = 0;
+  bool initialized = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    if (!initialized) {
+      heightA = MediaQuery.of(context).size.height / 2;
+      heightB = MediaQuery.of(context).size.height / 2;
+      scoreA = 5;
+      scoreB = 5;
+      initialized = true;
+    }
+
+    return Scaffold(
+      body: Column(
+        children: [
+          InkWell(
+            onTap: () {
+              HapticFeedback.vibrate();
+              setState(() {
+                if (heightB + 50 < MediaQuery.of(context).size.height) {
+                  heightB += 50;
+                  heightA -= 50;
+                  scoreB += 5;
+                  scoreA -= 5;
+                } else {
+                  heightB = MediaQuery.of(context).size.height;
+                  heightA = 0;
+                  print("B won");
+                }
+              });
+            },
+            child: Container(
+              color: Colors.blueAccent,
+              width: double.infinity,
+              height: heightB,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Player B",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    Text(
+                      scoreB.toString(),
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              HapticFeedback.lightImpact();
+              setState(() {
+                if (heightA + 50 < MediaQuery.of(context).size.height) {
+                  heightA += 50;
+                  heightB -= 50;
+                  scoreA += 5;
+                  scoreB -= 5;
+                } else {
+                  heightA = MediaQuery.of(context).size.height;
+                  heightB = 0;
+                  print("A won");
+                }
+              });
+            },
+            child: Container(
+              color: Colors.redAccent,
+              width: double.infinity,
+              height: heightA,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Player A",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    Text(
+                      scoreA.toString(),
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
